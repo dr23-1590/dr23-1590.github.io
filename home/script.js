162987b2d1af355967d5779dec7a16de4405b7f3
dr23-1590 <dr23-1590@unphu.edu.do>
2025-03-12 20:20:45 -0400

    Add files via upload

---
+++ home/script.js
@@ -0,0 +1,8 @@
+document.getElementById("toggleDarkMode").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
+
+
+function mostrarAlerta() {
+    alert("Gorras a 1x$650, 2x$990");
+}
